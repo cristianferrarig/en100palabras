7903a49279856beb6be97f0c5c0273a5db7417b1
Cristian Ferrari <cristianferrarig@gmail.com>
2015-11-16 20:45:47 -0300

    Configure bower and adding bootstrap 3

--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,24 @@ module RailsBase
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join('vendor', 'bower')
+    require './lib/sass_extensions' if defined?(Sass)
 
     config.generators do |g|
       g.template_engine :slim
     end
 
-    require './lib/sass_extensions' if defined?(Sass)
 
+    # Bower asset paths
+    root.join('vendor', 'bower').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+
+    # Precompile Bootstrap fonts
+    # config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
 
   end
 end
