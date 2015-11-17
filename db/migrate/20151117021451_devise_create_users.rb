329916acd65d3f241213c504e50f13ac95dbe366
Cristian Ferrari <cristianferrarig@gmail.com>
2015-11-16 23:43:47 -0300

    Add additional attributes for model 'User'

--- db/migrate/20151117021451_devise_create_users.rb
+++ db/migrate/20151117021451_devise_create_users.rb
@@ -2,12 +2,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
+      t.string :username            null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Basic profile
-      t.string :about
+      t.string :name
+      t.string :url
+      t.text   :about
       t.string :avatar
       t.string :cover
 
@@ -39,6 +41,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :users, :username,             unique: true
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
