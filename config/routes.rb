bd1c5378227a31704b56a90ced98bcef12931169
Cristian Ferrari <cristianferrarig@gmail.com>
2015-11-19 16:19:09 -0300

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :regions
-  resources :contests
-  resources :stories
+
+  resources :regions do
+    resources :contests do
+      resources :stories
+    end
+  end
+
   resources :cities
   resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
