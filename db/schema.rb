94d38183dfdb83e3ff7c045786c00906ac7e53c2
Cristian Ferrari <cristianferrarig@gmail.com>
2015-11-19 16:16:43 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117021451) do
+ActiveRecord::Schema.define(version: 20151119183058) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "contests", force: :cascade do |t|
+    t.integer  "year",       limit: 4
+    t.integer  "region_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "contests", ["region_id"], name: "index_contests_on_region_id", using: :btree
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.text     "title",      limit: 65535
+    t.text     "body",       limit: 65535
+    t.string   "author",     limit: 255
+    t.integer  "age",        limit: 4
+    t.integer  "city_id",    limit: 4
+    t.integer  "place_id",   limit: 4
+    t.integer  "contest_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "stories", ["city_id"], name: "index_stories_on_city_id", using: :btree
+  add_index "stories", ["contest_id"], name: "index_stories_on_contest_id", using: :btree
+  add_index "stories", ["place_id"], name: "index_stories_on_place_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +74,8 @@ ActiveRecord::Schema.define(version: 20151117021451) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "contests", "regions"
+  add_foreign_key "stories", "cities"
+  add_foreign_key "stories", "contests"
+  add_foreign_key "stories", "places"
 end
