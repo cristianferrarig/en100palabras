34ceb1b16424f310d064f1c4dd768194b4b5b5da
Cristian Ferrari <cristianferrarig@gmail.com>
2015-11-19 16:17:39 -0300

    Remove require_user_signed_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,23 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
-  private
-
-  def require_user_signed_in
-    unless user_signed_in?
-
-      # If the user came from a page, we can send them back.
-      # Otherwise, send them to the root path.
-      if request.env['HTTP_REFERER']
-        fallback_redirect = :back
-      elsif defined?(root_path)
-        fallback_redirect = root_path
-      else
-        fallback_redirect = "/"
-      end
-
-      redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
-    end
-  end
-
 end
